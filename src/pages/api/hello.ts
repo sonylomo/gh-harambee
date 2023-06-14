2ee008f7cf81747811989c6b5fee8627980d37c1
sonylomo <sonylomo1@gmail.com>
2023-06-14 12:18:15 +0300

    fix: type errors

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]"
 import type { NextApiRequest, NextApiResponse } from "next"
